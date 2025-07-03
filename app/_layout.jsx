3f946742cdf3a39292212db4c7be92a5bc2c80af
mrjahsan <rogzephyraus@outlook.com>
2025-07-03 13:14:53 +1000

    Changed default layout of the boiler plate

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,25 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+  return (
+    <Stack
+      screenOptions = {{
+        headerStyle: {
+          backgroundColor: "#ff8c00",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          PaddingTop: 10,
+          backgroundColor: "#fff",
+        },
+      }}
+    />
+  );
+};
+
+export default RootLayout;
